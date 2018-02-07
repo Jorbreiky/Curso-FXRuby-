2ba284025e8fdb3597045fcf7ca9dc954b4bf2fa
jorbreiky <jorgeitch@hotmail.com>
2018-02-06 22:05:57 -0600

    Agregar clase

---
+++ Combo.rb
@@ -0,0 +1,32 @@
+require 'fox16'
+include Fox
+
+class Combo < FXMainWindow
+  def initialize(app)
+    super(app,"Curso FXRuby", :width=>500, :height=>500)
+
+    fuente = FXFont.new(app,"Modern, 120, BOLD,0")
+    combo = FXComboBox.new(self, 3, :opts=> LAYOUT_EXPLICIT, :width=>200, :height=>30, :x=>10, :y=>10)
+    combo.numVisible = 3
+    combo.editable = false
+    combo.font = fuente
+    combo.appendItem("Opcion 1")
+    combo.appendItem("Opcion 2")
+    combo.appendItem("Opcion 3")
+
+    combo.connect(SEL_COMMAND)do
+      puts combo.text
+    end
+
+  end
+
+  def create
+    super
+    show(PLACEMENT_SCREEN)
+  end
+end
+
+app = FXApp.new
+Combo.new(app)
+app.create
+app.run
